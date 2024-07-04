2b0ec8fdfb0692fddbe2d7b047c2930db51a3f7b
kevinsourcode <kevnroy1@gmail.com>
2024-07-04 17:39:08 +0530

    connecting to a tracker, adding comments and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learning how to make your own torrent client using Node JS
+
+step 1: parse the bencoded metainfo file, to extract metadata.
+step 2: connect to the tracker.
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 
 import fs from 'fs';
-import bencode from 'bencode';
+import bencode from 'bencode'; //can write our own bencode parser
 
+import dgram from 'dgram'
+import { Buffer } from 'buffer';
+
+//parsing the bencoded metainfo file
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 
 // Convert the announce property to a buffer and then to a string
-const announceUrl = Buffer.from(torrent.announce).toString('utf8');
-console.log(announceUrl);
+const announceUrl = new URL(Buffer.from(torrent.announce).toString('utf8'));
+console.log(announceUrl)
 
+const socket = dgram.createSocket('udp4'); //creating a socket connection
+const myMsg = Buffer.from('hello?', 'utf8'); //messages can be only sent as a buffer
 
+socket.send(myMsg, 0, myMsg.length, announceUrl.port, announceUrl.host, () => {});
+
+//handling incoming messages
+socket.on('message', msg => {
+    console.log('message is', msg);
+  });
