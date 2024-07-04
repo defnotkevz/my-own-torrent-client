e2fa87119a5a791b0af1c383d1fe0d391744489b
kevinsourcode <kevnroy1@gmail.com>
2024-07-04 17:59:46 +0530

    UDP tracker protocol and message format

--- index.js
+++ index.js
@@ -12,13 +12,3 @@ const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 // Convert the announce property to a buffer and then to a string
 const announceUrl = new URL(Buffer.from(torrent.announce).toString('utf8'));
 console.log(announceUrl)
-
-const socket = dgram.createSocket('udp4'); //creating a socket connection
-const myMsg = Buffer.from('hello?', 'utf8'); //messages can be only sent as a buffer
-
-socket.send(myMsg, 0, myMsg.length, announceUrl.port, announceUrl.host, () => {});
-
-//handling incoming messages
-socket.on('message', msg => {
-    console.log('message is', msg);
-  });
